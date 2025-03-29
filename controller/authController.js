0ca1533b27c6dc16e39ff0a4066cc305ec265a53
Waynexlink <wm500972@gmail.com>
2025-03-29 12:50:20 +0100

    login_Route

--- controller/authController.js
+++ controller/authController.js
@@ -2,15 +2,44 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 const AppError = require("../utils/appError");
+const config = require("../config/config");
 
-const registerUser = async (req, res) => {
+const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
     //search database for existing user
-    const existingUser = User.findOne({ email });
-    if(existingUser) return next(new AppError('User already exist',400))
-      
-    const newUser = await User.create({name,email,password})
-    res.status(201).json({status:"success",message:"User created successfully"})
-  } 
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return next(new AppError("User already exist", 400));
+
+    const newUser = await User.create({ name, email, password });
+    res
+      .status(201)
+      .json({ status: "success", message: "User created successfully" });
+  } catch (err) {
+    next(err);
+  }
 };
+
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    //seach database if user exists
+    const user = (await User.findOne({ email })).select("+password");
+    if (!user) return next(new AppError("Invalid email or password", 400));
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch)
+      return next(new AppError("Invalid email or password", 400));
+
+    const token = jwt.sign({ userId: user._id }, config.jwtSecret, {
+      expiresIn: "1h",
+    });
+    res
+      .status(200)
+      .json({ status: " success", message: "Login successful", token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const {
+  validateUser,
+  handleValidationError,
+} = require("../middleware/validateUser");
 
+const { registerUser, loginUser } = require("../controller/authController");
 const router = express.Router();
 
 //handling register and login route
-router.post("/register");
-router.post("/login");
+router.post("/register", validateUser, handleValidationError, registerUser);
+router.post("/login", loginUser);
 module.exports = router;
