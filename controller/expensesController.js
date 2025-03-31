09d54d544478cf9b2d503fe1231a6a9e7525e1c7
Waynexlink <wm500972@gmail.com>
2025-03-31 07:41:06 +0100

    seventh-commit

--- controller/expensesController.js
+++ controller/expensesController.js
@@ -1,14 +1,43 @@
+const Expense = require("../models/expensesModel");
 const Expenses = require("../models/expensesModel");
 
 const getExpenses = async (req, res) => {
   try {
+    const { period, from, to, page = 1, limit = 10 } = req.query;
+    const query = { user: req.user._id };
+
+    if (period) {
+      const periods = {
+        lastWeek: Date.now(now.setDate(now.setDate() - 7)),
+        lastMonth: Date.now(now.setDate(now.setDate() - 1)),
+        last3Month: Date.now(now.setDate(now.setDate() - 3)),
+      };
+      query.date ={$gte ,periods[period]}
+    }else if(from && to) {
+      query.date = {$gte:new Date(from), $lte:new Date(to)}
+    }
+    //pagination
+    const skip = (parseInt(page)-1) *parseInt(limit)
+    
+    //fetch Data
+    const expense = await Expense.find(query).sort({date:.1}).limit(parseInt(limit)).skip(skip)
+    
+    
+    //send response 
+    res.status(200).json({
+      status: "success",
+      total: totalExpenses,
+      page: parseInt(page),
+      limit: parseInt(limit),
+      data: expense,
+    });
   } catch (error) {
     next(error);
   }
 };
 const createExpense = async (req, res) => {
   try {
-    const {} = req.body;
+    const { title, amount, categories, date } = req.body;
     const expense = await expense.create({});
     res
       .status(201)
@@ -27,7 +56,7 @@ const getExpense = async (req, res) => {
 
     res
       .status(200)
-      .json({ status: "success", Message: "Successfully deleted", expense });
+      .json({ status: "success", Message: "Expense retrived", expense });
   } catch (error) {
     next(error);
   }
@@ -38,9 +67,9 @@ const editExpense = async (req, res) => {
       _id: req.params.id,
       user: req.user._id,
     });
-    if (!expense) return next(new AppError("Expense not found ", 401));
+    if (!expense) return next(new AppError("Expense not found ", 404));
     //add req.body to task
-    Object.assign(task, req.body, { new: true, runValidators: true });
+    Object.assign(expense, req.body, { new: true, runValidators: true });
 
     await expense.save();
     res.status(201).json({ status: "success", Message: "Successfully edited" });
@@ -54,7 +83,7 @@ const deleteExpense = async (req, res) => {
       _id: req.params.id,
       user: req.user._id,
     });
-    if (!expense) return next(new AppError("Expense not found ", 401));
+    if (!expense) return next(new AppError("Expense not found ", 404));
 
     await expense.deleteOne();
     res
@@ -65,4 +94,4 @@ const deleteExpense = async (req, res) => {
   }
 };
 
-module.exports = { deleteExpense, editexpenses };
+module.exports = { deleteExpense, editExpense, getExpense, createExpense };
